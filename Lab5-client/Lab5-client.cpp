d7c51d0082f4dbf23106c7925e327cbbb39820ac
Ryzhov Alexandr <ryzhov.ae@avgd.pro>
2022-04-19 01:15:33 +0400

    первый запуск

--- Lab5-client/Lab5-client.cpp
+++ Lab5-client/Lab5-client.cpp
@@ -1,20 +1,101 @@
-﻿// Lab5-client.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
 #include <iostream>
+#include <windows.h>
+#include <string>
+
+#define BUFSIZE 1024
+
+using namespace std;
+
+char* buf = new char[BUFSIZE];
+DWORD dwBytes;
+
+HANDLE createPipe();
+string readData(HANDLE hPipe);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russian");
+
+	printf("Покупатель начал свои покупки. process id=%d", GetCurrentProcessId());
+
+	int duration = rand() % 9000 + 1000;
+	Sleep(duration);
+
+	int sum = rand() % 14000 + 1000;
+	printf("Покупатель набрал товар на сумму %d и пошел к кассам", sum);
+
+	HANDLE hPipe = createPipe();
+
+	string s_sum = to_string(sum);
+	DWORD dwBytes;
+	if (!WriteFile(hPipe, s_sum.c_str(), size(s_sum), &dwBytes, NULL)) {
+		printf("Ошибка при передаче данных на сервер.\n");
+		system("pause");
+		exit(1);
+	}
+
+	string msg = readData(hPipe);
+	printf("Порядковый номер клиента: %s", msg.c_str());
+
+	msg = readData(hPipe);
+	printf("Номер кассира: %s", msg.c_str());
+
+	msg = readData(hPipe);
+	printf("Номер в очереди: %s", msg.c_str());
+
+	msg = readData(hPipe);
+	if (!strcmp(msg.c_str(), "Done"))
+	{
+		printf("Покупка успешно завершена.\n");
+	}
+	else
+	{
+		printf("Получена неизвестная команда - %s.\n", msg.c_str());
+	}
+
+	delete buf;
+	CloseHandle(hPipe);
+
+
+	system("pause");
+	return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+HANDLE createPipe()
+{
+	while (true)
+	{
+		HANDLE hPipe = CreateFile(TEXT("\\\\.\\pipe\\shop"), GENERIC_READ | GENERIC_WRITE, NULL, NULL, OPEN_EXISTING, NULL, NULL);
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+		if (hPipe != INVALID_HANDLE_VALUE)
+		{
+			return hPipe;
+		}
+
+		if (GetLastError() != ERROR_PIPE_BUSY) {
+			printf("Не могу открыть именованный канал.\n");
+			system("pause");
+			exit(1);
+		}
+
+		if (!WaitNamedPipe(TEXT("\\\\.\\pipe\\shop"), 10000)) {
+			printf("Не дождались открытия магазина в течение 10 секунд");
+			system("pause");
+			exit(1);
+		}
+	}
+}
+
+string readData(HANDLE hPipe)
+{
+	if (!ReadFile(hPipe, buf, BUFSIZE, &dwBytes, NULL)) {
+		printf("Ошибка получения данных.\n");
+		return NULL;
+	}
+
+	string msg(buf, 0, dwBytes);
+	return msg;
+}
+
+
--- Lab5/Lab5-server.cpp
+++ Lab5/Lab5-server.cpp
@@ -28,34 +28,81 @@
 
 using namespace std;
 
-// Данные кассы
+DWORD WINAPI worker(LPVOID lpParam);
+HANDLE createPipe();
+void waitingForCustomer(HANDLE& pipe);
+
+// Покупатель
+struct Customer
+{
+	int id;
+	HANDLE hPipe;
+	int sum;
+
+	Customer(int id, HANDLE hPipe, int sum) {
+		this->id = id;
+		this->hPipe = hPipe;
+		this->sum = sum;
+	};
+
+	void send(string message)
+	{
+
+	}
+};
+
+// Кассир
 struct Cashier
 {
 	int id;
 	queue<Customer> queue;
-	HANDLE mutex;
+	HANDLE mutex = NULL;
+	HANDLE thread = NULL;
+	int total_sum = 0;
 
-	Cashier(int id) {
+	Cashier(int id, Customer customer) {
 		this->id = id;
-
+		this->queue.push(customer);
 		this->mutex = CreateMutex(NULL, 0, NULL);
 		if (this->mutex) {
 			printf("Ошибка при создании мьютекса: %d\n", GetLastError());
+			system("pause");
 			exit(1);
 		}
 	};
+
+	~Cashier()
+	{
+		CloseHandle(this->mutex);
+		CloseHandle(this->thread);
+		this->mutex = NULL;
+	}
+
+	bool lockQueue()
+	{
+		DWORD dwWaitResult = WaitForSingleObject(this->mutex, INFINITE);
+		return (dwWaitResult == WAIT_OBJECT_0);
+	}
+
+	void unlockQueue()
+	{
+		ReleaseMutex(this->mutex);
+	}
+
+	void startWork()
+	{
+		thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)worker, (LPVOID)this, 0, NULL);
+		if (thread == NULL)
+		{
+			printf("Ошибка при создании потока: %d\n", GetLastError());
+			system("pause");
+			exit(1);
+		}
+	}
 };
 
-struct Customer
-{
-	int id;
-	HANDLE pipe;
-
-	Customer(int id, HANDLE pipe) {
-		this->id = id;
-		this->pipe = pipe;
-	};
-};
+Cashier chooseCashier(Customer customer);
+bool addCashier(Customer customer);
 
 vector<Cashier> cashiers;
 int cashiersMaxCount;
@@ -72,61 +119,39 @@ int main()
 	printf("Введите количество кассиров X: ");
 	cin >> cashiersMaxCount;
 
-	printf("Магазин начал свою работу.");
+	printf("Магазин начал свою работу. Ждем покупателей.");
 
 	while (true)
 	{
-		HANDLE pipe = createPipe();
-		waitingForCustomer(pipe);
+		// создаем канал и ждем очередного покупателя
+		HANDLE hPipe = createPipe();
+		waitingForCustomer(hPipe);
 
 		// Читаем информацию по сумме
-		if (!ReadFile(pipe, buf, BUFSIZE, &dwBytes, NULL)) {
+		if (!ReadFile(hPipe, buf, BUFSIZE, &dwBytes, NULL)) {
 			printf("Ошибка получения данных от клиента.\n");
+			system("pause");
 			exit(1);
 		}
 
 		string msg(buf, 0, dwBytes);
+		int sum = atoi(msg.c_str());
 
-		int cashierIndex = rand() % cashiers.size();
-		Cashier* cashier = new Cashier(pipe, 0, ++customerCount);
-
-		if (msg == "Стоматолог")
-			d->doctor_id = 0;
-		else if (msg == "Хирург")
-			d->doctor_id = 1;
-		else if (msg == "Терапевт")
-			d->doctor_id = 2;
-		else if (msg == "Stop") {
-			CloseHandle(pipe);
-			printf("Программа завершает работу...\n");
-			break;
-		}
-		else {
-			printf("Ошибка при вводе данных на стороне клиента.\n");
-			exit(1);
-		}
-
-		cashiers.push_back(*cashier);
-		HANDLE thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)worker, (LPVOID)&patients.at(patients.size() - 1), 0, NULL);
-		if (thread == NULL) {
-			printf("Ошибка при создании потока: %d\n", GetLastError());
-			exit(1);
-		}
-
-
+		Customer customer(++customerCount, hPipe, sum);
+		Cashier cashier = chooseCashier(customer);
 	}
-
 }
 
 HANDLE createPipe()
 {
-	// Создаём именованный канал для общения клиентов
+	// Создаём именованный канал для следующего покупателя
 	HANDLE pipe = CreateNamedPipe(TEXT("\\\\.\\pipe\\shop"), PIPE_ACCESS_DUPLEX,
 		PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
 		PIPE_UNLIMITED_INSTANCES, BUFSIZE, BUFSIZE, 0, NULL);
 
 	if (pipe == INVALID_HANDLE_VALUE) {
 		printf("Ошибка при создании именованного канала: %d\n", GetLastError());
+		system("pause");
 		exit(1);
 	}
 
@@ -144,8 +169,75 @@ void waitingForCustomer(HANDLE& pipe)
 	}
 }
 
-void addCashier()
+Cashier chooseCashier(Customer customer)
 {
-	Cashier* cashier = new Cashier(pipe, 0, ++customerCount);
+	// если еще нет ни одного кассира за кассой зовем первого
+	if (cashiers.size() == 0) {
+		addCashier(customer);
+		return cashiers.at(cashiers.size() - 1);
+	}
+
+	// если кассиры есть, выбираем случайного
+	int cashierIndex;
+	cashierIndex = rand() % cashiers.size();
+	Cashier cashier = cashiers.at(cashierIndex);
+
+	if (cashier.lockQueue())
+	{
+
+		if (cashier.queue.size() < 3 || !addCashier(customer))
+		{
+			cashier.queue.push(customer);
+			return cashier;
+		}
+
+		cashier.unlockQueue();
+	}
 }
+
+bool addCashier(Customer customer)
+{
+	if (cashiers.size() < cashiersMaxCount)
+	{
+		int cashier_id = cashiers.size() + 1;
+		Cashier cashier(cashier_id, customer);
+		cashiers.push_back(cashier);
+		return true;
+	}
+
+	return false;
+}
+
+DWORD WINAPI worker(LPVOID lpParam)
+{
+	Cashier* cashier = (Cashier*)lpParam;
+
+	while (true)
+	{
+		if (cashier->lockQueue())
+		{
+			if (cashier->queue.size() == 0) {
+				Sleep(1000);
+				continue;
+			}
+			Customer* customer = &cashier->queue.front();
+			cashier->queue.pop();
+
+			cashier->unlockQueue();
+
+
+			printf("Обслуживаем покупателя %d на кассе %d", customer->id, cashier->id);
+			Sleep(2000);
+			cashier->total_sum += customer->sum;
+
+			printf("Обслужили покупателя %d на кассе %d", customer->id, cashier->id);
+			customer->send("Done");
+			delete customer;
+		}
+	}
+	printf("Завершаем обслуживание на кассе %d", cashier->id);
+	system("pause");
+	ExitThread(0);
+}
+
 
